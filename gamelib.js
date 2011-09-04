bc3dfee88108e6321999bd08dbcc8e8efa336e3d
condor <mdiebolt@gmail.com>
2011-09-04 18:56:49 -0400

    Documentation

--- gamelib.js
+++ gamelib.js
@@ -5154,22 +5154,17 @@ collisions so is probably best suited to particle effect like things.
     
    player.include(Movable)
   
-   player.I.velocity
-=> {x: 0, y: 0}
+=> `player velocity is {x: 0, y: 0} | player position is {x: 0, y: 0}`
 
    player.update()
-   player.I.velocity
-=> {x: 1, y: 0}
+=> `player velocity is {x: 1, y: 0} | player position is {x: 1, y: 0}` 
 
    player.update()
-   player.I.velocity
-=> {x: 2, y: 0}
-
-   # now we've hit our maxSpeed
-   # so our velocity won't increase
+=> `player velocity is {x: 2, y: 0} | player position is {x: 3, y: 0}`   
+   
+   # we've hit our maxSpeed so our velocity won't increase
    player.update()
-   player.I.velocity
-=> {x: 2, y: 0}
+=> `player velocity is {x: 2, y: 0} | player position is {x: 5, y: 0}`
 </pre></code>
 
 @name Movable
