4371d84290ee281dbb0a9807abb767ce4bab49e9
condor <mdiebolt@gmail.com>
2011-09-04 18:16:59 -0400

    durable

--- gamelib.js
+++ gamelib.js
@@ -4368,9 +4368,27 @@ Drawable = function(I, self) {
   };
 };;
 /**
-The Durable module deactives GameObjects after a specified duration.
+The Durable module deactives a <code>GameObject</code> after a specified duration.
 If a duration is specified the object will update that many times. If -1 is
 specified the object will have an unlimited duration.
+
+<code><pre>
+   enemy = GameObject
+     x: 50
+     y: 30
+     duration: 5
+    
+   enemy.include(Durable)
+  
+   enemy.I.active
+=> true
+  
+   5.times ->
+     enemy.update()
+   
+   enemy.I.active
+=> false
+</pre></code>
 
 @name Durable
 @module
@@ -4385,7 +4403,7 @@ Durable = function(I) {
   return {
     before: {
       update: function() {
-        if (I.duration !== -1 && I.age >= I.duration) {
+        if (I.duration !== -1 && I.age + 1 >= I.duration) {
           return I.active = false;
         }
       }
