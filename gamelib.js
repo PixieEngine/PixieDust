baa90a24f48c71524da7dc1621cd94b5b668a916
condor <mdiebolt@gmail.com>
2011-09-06 19:55:51 -0400

    movable

--- gamelib.js
+++ gamelib.js
@@ -5825,7 +5825,39 @@ Movable = function(I) {
   };
   return (typeof exports !== "undefined" && exports !== null ? exports : this)["ResourceLoader"] = ResourceLoader;
 })();;
-var Rotatable;
+/**
+The Rotatable module rotates the object
+based on its rotational velocity.
+
+<code><pre>
+   player = GameObject
+     x: 0
+     y: 0
+     rotationalVelocity: Math.PI / 64
+
+   player.include(Rotatable)
+
+   player.I.rotation
+=> 0
+
+   player.update()
+
+   player.I.rotation
+=> 0.04908738521234052 # Math.PI / 64
+
+   player.update()
+   
+   player.I.rotation
+=> 0.09817477042468103 # 2 * (Math.PI / 64)
+</pre></code>
+
+@name Rotatable
+@module
+@constructor
+
+@param {Object} I Instance variables
+@param {Core} self Reference to including object
+*/var Rotatable;
 Rotatable = function(I) {
   I || (I = {});
   Object.reverseMerge(I, {
