a6c9db051ec35467fe359408256cb1e51e1cce8a
condor <mdiebolt@gmail.com>
2012-03-26 12:03:10 -0400

    ClampBounds docs

--- gamelib.js
+++ gamelib.js
@@ -5046,6 +5046,61 @@ Camera.ZSort = function(I, self) {
   return {};
 };
 ;
+/**
+The ClampBounds module adds a check to make sure
+that the including GameObject doesn't move outside
+the viewport.
+
+<code><pre>
+  # create a player and include ClampBounds
+  player = GameObject
+    includedModules: ["ClampBounds"]
+    width: 5
+    height: 17
+
+  # put the player outside the viewport
+  player.I.x = -400
+  player.I.y = 1000
+
+  # update the player so ClampBounds can set 
+  # his position back inside the viewport.
+  player.update()
+
+  # x, y position is based on the center point so
+  # the position the player is set to is based on
+  # half their width and height
+  player.I.x
+  # => 2.5 # half the player's width
+
+  player.I.y
+  # => 311.5 # The default App.height (320) - half the player's height
+</pre></code>
+
+@name Cooldown
+@module
+@constructor
+@param {Object} I Instance variables
+@param {Core} self Reference to including object
+*/
+var ClampBounds;
+
+ClampBounds = function(I, self) {
+  if (I == null) I = {};
+  Object.reverseMerge(I, {
+    x: 0,
+    y: 0,
+    width: 32,
+    height: 32
+  });
+  App || (App = {});
+  App.width || (App.width = 480);
+  App.height || (App.height = 320);
+  return self.bind("update", function() {
+    I.x = I.x.clamp(I.width / 2, App.width - I.width / 2);
+    return I.y = I.y.clamp(I.height / 2, App.height - I.height / 2);
+  });
+};
+;
 
 (function() {
   /**
