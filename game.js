2c4bbda51ab35abfb1ea7f96dc45b3874ae76644
condor <mdiebolt@gmail.com>
2012-05-21 15:01:35 -0400

    Modified in browser at pixie.strd6.com

--- game.js
+++ game.js
@@ -4277,9 +4277,9 @@ The Ageable module handles keeping track of an object's age.
     
     player.update(1)
     
-    #=> player.I.age == 1
-    
+    #=> player.I.age == 1   
 
+Ageable module
 @name Ageable
 @module
 @constructor
