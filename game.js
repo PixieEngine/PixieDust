a24b166d0e437dfe8c8b5f83053bfaac375d8c72
condor <mdiebolt@gmail.com>
2012-05-21 15:10:43 -0400

    Modified in browser at pixie.strd6.com

--- game.js
+++ game.js
@@ -4247,6 +4247,44 @@ Generate a random uuid.
 })();;
 ;
 ;
+/**
+The ActiveBounds module automatically destroys objects that
+are outside of the specified bounds. The default bounds are
+the dimensions of your game. Useful for bullet type objects.
+
+    bullet = GameObject
+      x: 10
+      y: 50
+      width: 20
+      height: 20
+      velocity: Point(120, 0)
+
+    bullet.include ActiveBounds
+
+    # => bullet will be removed when it
+         goes outside of the game bounds.
+    
+    bullet2 = GameObject
+      x: 50
+      y: 50
+      width: 30
+      height: 20
+      activeBounds: Rectangle
+        x: 30
+        y: 20
+        width: 100
+        height 100
+
+    # => bullet2 will be removed unless 30 <= I.x <= 130
+         and 20 <= I.y <= 120
+
+ActiveBounds module
+@name ActiveBounds
+@module
+@constructor
+@param {Object} I Instance variables
+@param {Core} self Reference to including object
+*/
 var ActiveBounds;
 
 ActiveBounds = function(I, self) {
@@ -4302,7 +4340,7 @@ Ageable = function(I, self) {
 /**
 The Bounded module is used to provide basic data about the
 location and dimensions of the including object. This module is included
-by default in <code>GameObject</code>.
+by default in `GameObject`.
 
     player = Core
       x: 10
