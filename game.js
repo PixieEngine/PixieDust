572a5a8dd797a3e65c56a7c8e883fa7f426d96dc
condor <mdiebolt@gmail.com>
2012-05-24 16:57:58 -0400

    Modified in browser at pixie.strd6.com

--- game.js
+++ game.js
@@ -7256,6 +7256,42 @@ Drawable = function(I, self) {
 
 Drawable.setSizeCallback = function(sprite) {};
 ;
+var DustEmitter, DustParticle;
+
+DustParticle = function(I, self) {
+  if (I == null) I = {};
+  Object.extend(I, {
+    color: 'rgb(100, 100, 100)',
+    duration: 0.2,
+    fadeOut: true,
+    maxSpeed: 90
+  });
+  I.velocity = [Point(-60, -30), Point(40, -15), Point(-20, -7), Point(60, -30), Point(40, -15), Point(20, -7)].rand();
+  I.acceleration = Point(0, 60);
+  return {};
+};
+
+DustEmitter = function(I) {
+  var self;
+  if (I == null) I = {};
+  Object.reverseMerge(I, {
+    duration: 3,
+    particleCount: 20,
+    batchSize: 5,
+    x: 0,
+    y: 0,
+    zIndex: 50,
+    generator: {
+      includedModules: ["DustParticle"],
+      radius: function(n) {
+        return [2, 3, 1].wrap(n);
+      }
+    }
+  });
+  self = Emitter(I);
+  return self;
+};
+;
 
 (function() {
   var Easing, polynomialEasings;
