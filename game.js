eab8f8e5e1a6d79f0c4eab02d7d1f83b6f0d0831
condor <mdiebolt@gmail.com>
2012-05-27 02:06:48 -0400

    Modified in browser at pixie.strd6.com

--- game.js
+++ game.js
@@ -9771,26 +9771,26 @@ TextEffect = function(I) {
   });
   self = GameObject(I);
   self.bind("update", function() {
+    if (I.rotationalVelocity != null) I.rotation += I.rotationalVelocity;
     return I.alpha = (1 - (I.age / I.duration)).clamp(0, 1);
   });
   self.unbind("draw");
   self.bind("draw", function(canvas) {
-    var width;
     if (!I.color.channels) I.color = Color(I.color);
     if (!I.textShadow.channels) I.textShadow = Color(I.textShadow);
     I.color.a = I.alpha;
     I.textShadow.a = I.alpha;
-    width = canvas.measureText();
+    I.width = canvas.measureText(I.text);
     canvas.font(I.font);
     canvas.drawText({
       color: I.textShadow,
-      x: 1 - width / 2,
+      x: 1 - I.width,
       y: 1,
       text: I.text
     });
     return canvas.drawText({
       color: I.color,
-      x: 0 - width / 2,
+      x: 0 - I.width,
       y: 0,
       text: I.text
     });
